8781e000466bccbc6679150be323d880b6d09df1
ozkanmustafa <mustafaozkan9842@gmail.com>
2018-03-19 12:40:36 +0300

    ss

--- views/frontend/default/_index.php
+++ views/frontend/default/_index.php
@@ -2,7 +2,7 @@
 use kouosl\theme\helpers\Html;
 use kouosl\theme\widgets\Portlet;
 
-$this->title = 'Index Sample';
+$this->title = 'Index MEnu';
 $data['title'] = Html::encode($this->title);
 $this->params['breadcrumbs'][] = $this->title;
 
