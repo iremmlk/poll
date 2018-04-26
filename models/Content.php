b56ec895a099474a2ade77f7b978f0a21404bf39
Yasemin Gökberk <yasemingokberk@gmail.com>
2018-04-26 10:15:49 +0300

    .

---
+++ models/Content.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace kouosl\blog\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "content".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property string $picture
+ *
+ * @property data[] $data
+ */
+class Blog extends \yii\db\ActiveRecord
+{
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'content';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'description'], 'required'],
+            [['description', 'picture'], 'string'],
+            [['title'], 'string', 'max' => 200],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'picture' => 'Picture',
+            'body' => 'Body',
+            'created_at' =>'Created At',
+            'created_by' =>'Created By',
+            'updated_at' => 'Updated At',
+            'updated_by' => 'Updated By',
+            'category_id' => 'Category Id',
+            'category' => 'Category'
+        ];
+    }
+
+    public function getImagePath(){
+        return sprintf("%s/blog/%s",Yii::getAlias ( '@data' ),$this->picture);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getData()
+    {
+        return $this->hasMany(BlogData::className(), ['blog_id' => 'id']);
+    }
+}
